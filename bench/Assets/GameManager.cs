d5a588a89bc0c7556e1b9c20eb82e4105e46a5d8
vivogd <aheilweil@gmail.com>
2020-01-19 11:54:54 +0200

    Move Bench
    Move bench farward
    Changed some food and coin materials
    Dimmed the env.

---
+++ bench/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            _instance = this;
+        }
+    }
+
+    static public int score = 1000;
+    [SerializeField] GameObject cubePrefab; 
+    private void Start() {
+        
+    }
+
+
+    public  void SpawnPigeon() {
+        Instantiate(cubePrefab, Vector3.zero, Quaternion.identity, transform);
+    }
+}
